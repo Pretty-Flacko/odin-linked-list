482364e0160dcec94014b9119c9f14ab92df94ac
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2026-04-01 02:44:41 +0400

    Add append function

--- linked-list.js
+++ linked-list.js
@@ -9,4 +9,21 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+    }
+
+    current.nextNode = newNode;
+  }
 }
